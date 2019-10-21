9cc96c2a3b8a90fe639bd52d5872f20f5b9bfb41
Devin Schwab <digidevin@gmail.com>
2019-10-21 17:04:44 -0400

    :sparkles: Added draw options for ball filter

--- vision_filter/vision_filter/ui/draw_options_editor.py
+++ vision_filter/vision_filter/ui/draw_options_editor.py
@@ -12,6 +12,9 @@ class DrawOptionsEditor:
         self.draw_ball_detections_discrete: bool = True
         self.ball_detection_discrete_color: List[float] = [1.0, 0.65, 0.0, 1.0]
 
+        self.draw_ball_filter_discrete: bool = True
+        self.ball_filter_discrete_color: List[float] = [0.416, 0.353, 0.804, 1.0]
+
     def __call__(self):
         if not self.visible:
             return
@@ -26,6 +29,16 @@ class DrawOptionsEditor:
             if self.draw_ball_detections_discrete:
                 changed, self.ball_detection_discrete_color = imgui.color_edit4(
                     "Discrete ball color", *self.ball_detection_discrete_color
+                )
+
+        expanded, _ = imgui.collapsing_header("Ball Filter Options")
+        if expanded:
+            _, self.draw_ball_filter_discrete = imgui.checkbox(
+                "Draw discrete", self.draw_ball_filter_discrete
+            )
+            if self.draw_ball_filter_discrete:
+                changed, self.ball_filter_discrete_color = imgui.color_edit4(
+                    "Discrete ball color", *self.ball_filter_discrete_color
                 )
 
         imgui.end()
